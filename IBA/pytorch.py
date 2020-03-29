fixed gradient NaN issue on readout training

IBA/pytorch.py
@@ -245,7 +245,6 @@ class IBA(nn.Module):
         self._buffer_capacity = None  # Filled on forward pass, used for loss
         self.sigma = sigma
         self.estimator = estimator or TorchWelfordEstimator()
-        self.device = None
         self._estimate = False
         self._mean = feature_mean
         self._std = feature_std
@@ -316,6 +315,8 @@ class IBA(nn.Module):
         We use this point also to estimate the distribution of `x` passing through the layer.
         """
         if self._restrict_flow:
+            if self.alpha is None:
+                raise RuntimeWarning("Alpha not initialized. Run _init() before using the bottleneck.")
             return self._do_restrict_information(x, self.alpha)
         if self._estimate:
             self.estimator(x)
@@ -352,8 +353,6 @@ class IBA(nn.Module):
 
     def _do_restrict_information(self, x, alpha):
         """ Selectively remove information from x by applying noise """
-        if self.alpha is None:
-            raise RuntimeWarning("Alpha not initialized. Run _init() before using the bottleneck.")
 
         if self._mean is None:
             self._mean = self.estimator.mean()
@@ -378,9 +377,10 @@ class IBA(nn.Module):
         mu = x_norm * lamb
 
         # Sample new output values from p(z|x)
-        eps = mu.data.new(mu.size()).normal_()
-        z_norm = x_norm * lamb + (1-lamb) * eps
+        eps = lamb.data.new(lamb.size()).normal_()
+        z_norm = mu + (1-lamb) * eps
         self._buffer_capacity = self._calc_capacity(mu, log_var) * self._active_neurons
+        self._buffer_alpha = alpha.clone().detach()
 
         # Denormalize z to match original magnitude of x
         z = z_norm * self._std + self._mean
@@ -520,7 +520,7 @@ class IBA(nn.Module):
                           f"samples. Might not be enough! We recommend 10.000 samples.")
         std = self.estimator.std()
         self._active_neurons = self.estimator.active_neurons(active_neurons_threshold).float()
-        self._std = torch.max(std, min_std*torch.ones_like(std))
+        self._std = torch.max(std, torch.tensor(min_std, device=std.device))
 
         self._loss = []
         self._model_loss = []

IBA/pytorch_readout.py
@@ -45,6 +45,8 @@ class IBAReadout(IBA):
 
     def _build(self):
         super()._build()
+        # We do not need alpha
+        self.alpha = None
         # Use the estimators to get feature map dimensions
         features_in = sum(map(lambda e: e.shape[0], self._readout_estimators))
         features_out = self.estimator.shape[-3]
@@ -72,6 +74,22 @@ class IBAReadout(IBA):
             self._readout_hooks = [None for _ in self._readout_hooks]
 
     def analyze(self, input_t, model, mode='saliency', **kwargs):
+        """
+        Use the trained Readout IBA to find relevant regions in the input.
+        The input is passed through the network and the Readout Bottleneck restricts
+        the information flow. The capacity at each pixel is then returned as saliency
+        map, similar to the Per-Sample IBA.
+
+        Args:
+            input_t: input image of shape (1, C, H W)
+            model: the model containing the trained bottleneck
+            mode: how to post-process the resulting map: 'saliency' (default) or 'capacity'
+
+        Returns:
+            The heatmap of the same shape as the ``input_t``.
+
+        Additional arguments are ignored.
+        """
         if len(kwargs) > 0:
             warnings.warn(f"Additional arguments ({list(kwargs.keys())}) "
                           " are ignored in the Readout IBA.")
@@ -144,11 +162,12 @@ class IBAReadout(IBA):
         recorded feature maps in a 3-layer Readout Network to generate alpha.
         """
         # Run a nested pass to obtain feature maps, stored in self._readout_values
-        with self._nested_pass():
+        with self._enable_nested_pass(), torch.no_grad():
             self._model_fn(self._last_input)
 
         # Normalize using the estimators
-        readouts = [(r - e.mean()) / e.std()
+        min_std_t = torch.tensor(self.min_std, device=self._last_input.device)
+        readouts = [(r - e.mean()) / torch.max(e.std(), min_std_t)
                     for r, e in zip(self._readout_values, self._readout_estimators)]
 
         # Resize to fit shape of bottleneck layer
@@ -176,7 +195,7 @@ class IBAReadout(IBA):
         return alpha
 
     @contextmanager
-    def _nested_pass(self):
+    def _enable_nested_pass(self):
         """
         Context manager to pass the input once though the model in a nested pass to
         obtain the readout feature maps. These are then used as the input for the readout